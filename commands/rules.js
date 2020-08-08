56b5f871c4d49a8d800dc725df49f910d3715268
BoBsmil3Y <bygamusic@gmail.com>
2020-08-08 11:33:52 +0200

    Add a rules command

---
+++ commands/rules.js
@@ -0,0 +1,30 @@
+const {
+  MessageEmbed
+} = require("discord.js");
+
+module.exports = {
+
+  name: "rules",
+  description: "Envoi les règles du Discord sous forme d'image.",
+  usage: ".rules",
+  permission: false,
+  onlyAdmin: false,
+  execute(client, message, args) {
+
+    const rulesEmbed = new MessageEmbed()
+      .setColor("#0fbcf9")
+      .setAuthor("📑  |  Règles Discord")
+      .setDescription("*» Si vous êtes sur PC et que vous avez du mal à lire, cliquez sur l'image, puis cliquez sur 'Ouvrir l'original'*.")
+      .attachFiles(['./images/rules.jpg'])
+      .setImage('attachment://rules.jpg')
+      .setThumbnail(client.user.avatarURL())
+      .setTimestamp()
+      .setFooter("Myrith", client.user.avatarURL())
+      .setColor("#9980FA");
+
+    message.channel.send(rulesEmbed).catch(console.error);
+    message.delete();
+
+  }
+
+}
