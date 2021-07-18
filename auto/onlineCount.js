435974dc8ee8855b4c8aaf6ae12e91cb7d68107f
BoBsmil3Y <antoine-dupont@gmx.fr>
2021-07-18 23:49:26 +0200

    Fixing onlineCount issue and it's 100% accurate. memberCOunt got config options now

--- auto/onlineCount.js
+++ auto/onlineCount.js
@@ -1,16 +1,25 @@
 const config = require("../config.json");
+const request = require('request');
 
 module.exports = {
   description: "Compte le nombre de membre connecté en temps réel.",
-  run: client => {
+  run: async client => {
 
     const server = client.guilds.cache.get(config.channels.guild);
 
-    let onlineMembers = server.members.cache.filter(member => member.presence.status !== "offline").size;
+    let onlineMembers = await request('https://discord.com/api/v9/invites/' + config.invite + '?with_counts=true', function (error, response, body) {
 
-    if (onlineMembers < 2)
-      server.channels.cache.get(config.channels.onlineMember).setName(`🤵\u2009\u2009•\u2009\u2009${onlineMembers}\u2009\u2009en\u2009ligne.`);
-    else
-      server.channels.cache.get(config.channels.onlineMember).setName(`🤵\u2009\u2009•\u2009\u2009${onlineMembers}\u2009\u2009en\u2009lignes.`);
+      console.error('error:', error);
+      const res = JSON.parse(body);
+
+      onlineMembers = res.approximate_presence_count;
+
+      if (onlineMembers < 2)
+        server.channels.cache.get(config.channels.onlineMember).setName(`🤵\u2009\u2009•\u2009\u2009${onlineMembers}\u2009\u2009en\u2009ligne.`);
+      else
+        server.channels.cache.get(config.channels.onlineMember).setName(`🤵\u2009\u2009•\u2009\u2009${onlineMembers}\u2009\u2009en\u2009lignes.`);
+    });
+
+
   }
 };
--- auto/welcome.js
+++ auto/welcome.js
@@ -1,19 +1,20 @@
 const { MessageEmbed } = require("discord.js");
+const config = require("../config.json");
 
 module.exports = {
 
   description: "Souhaite la bienvenue aux nouveau membres.",
   run: (bot, newMember) => {
 
-    const guild = bot.guilds.cache.find(guild => guild.id === "659141545307406342");
-    const welcomeChannel = guild.channels.cache.find(channel => channel.id === "737770503246184532");
+    const guild = bot.guilds.cache.get(config.channels.guild);
+    const welcomeChannel = guild.channels.cache.get(config.channels.welcome);
 
     const embed = new MessageEmbed()
       .setTitle(`Un nouveau membre apparaît !`)
       .setDescription(`» Bienvenue à **${newMember.user.username}** sur le serveur Discord ! 🎉`)
-      .setColor("#ff7844")
-      .setThumbnail(newMember.user.avatarURL())
-      .setFooter("Myrith", bot.user.avatarURL())
+      .setColor("#F5BF5D")
+      .setThumbnail(newMember.user.displayAvatarURL())
+      .setFooter("Myrith", bot.user.displayAvatarURL())
       .setTimestamp();
 
     welcomeChannel.send(embed).then(message => {
