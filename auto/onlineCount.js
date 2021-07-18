75231610f6abc83ddf5a46fa79e836346fd78b23
BoBsmil3Y <antoine-dupont@gmx.fr>
2021-07-18 19:32:50 +0200

    Waiting to test it on the real server to see if the count is close to he truth

--- auto/onlineCount.js
+++ auto/onlineCount.js
@@ -1,15 +1,16 @@
+const config = require("../config.json");
+
 module.exports = {
-
   description: "Compte le nombre de membre connecté en temps réel.",
-  run: (client, oldPresence, newPresence) => {
+  run: client => {
 
-    if (newPresence.member.presence.status !== newPresence.member.presence.status || newPresence.user.bot) return;
+    const server = client.guilds.cache.get(config.channels.guild);
 
-    const guild = newPresence.member.guild;
-    const online = guild.members.cache.filter(member => (member.presence.status === ("online") | member.presence.status === ("idle") | member.presence.status === ("dnd")) && !member.user.bot);
+    let onlineMembers = server.members.cache.filter(member => member.presence.status !== "offline").size;
 
-    guild.channels.cache.get("744120600028774440").setName(`🤵\u2009\u2009•\u2009\u2009${online.size}\u2009\u2009en\u2009lignes.`);
-
+    if (onlineMembers < 2)
+      server.channels.cache.get(config.channels.onlineMember).setName(`🤵\u2009\u2009•\u2009\u2009${onlineMembers}\u2009\u2009en\u2009ligne.`);
+    else
+      server.channels.cache.get(config.channels.onlineMember).setName(`🤵\u2009\u2009•\u2009\u2009${onlineMembers}\u2009\u2009en\u2009lignes.`);
   }
-
 };
