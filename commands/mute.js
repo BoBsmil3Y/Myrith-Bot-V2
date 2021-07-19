8fa54d321f33819e6aa0cd4709e3215f13337832
BoBsmil3Y <antoine-dupont@gmx.fr>
2021-07-19 15:37:57 +0200

    Refactoring and adding config options

--- commands/mute.js
+++ commands/mute.js
@@ -1,7 +1,7 @@
-const {
-  MessageEmbed
-} = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 const ms = require("ms");
+const config = require("../config.json");
+
 
 module.exports = {
 
@@ -14,24 +14,26 @@ module.exports = {
 
     if (!message.guild.member(message.author).hasPermission('KICK_MEMBERS')) return;
 
-    const sanctionChannel = message.guild.channels.cache.find(channel => channel.id === "739032782679834717"); // A changer
-    const member = message.guild.members.cache.find(member => member.id === args[0].slice(3).replace(">", ""));
-    let muteRole = message.guild.roles.cache.find(role => role.name === "mute");
-
-    const muteTime = ms(args[1]);
-    let why = "";
-
-    for (let i = 2; i < args.length; i++) {
-      why = why + args[i] + " ";
-    }
-
-    if (!member || muteTime === 0 || !args[2]) {
+    if (args.length < 3) {
       message.author.send(`Tu as mal exécuté la commande. \nUsage : ${this.usage}. \nTa commande : ${message}`);
       message.delete();
       return;
     }
 
-    if (member.roles.cache.find(role => role.id === muteRole)) {
+    const sanctionChannel = message.guild.channels.cache.get(config.channels.sanction);
+    const muteRole = message.guild.roles.cache.get(config.roles.mute);
+    const member = message.guild.members.cache.get(message.mentions.users.first().id);
+    let trash = args.shift();
+    const muteTime = ms(args.shift());
+    const reason = args.join(' ');
+
+    if (!member || muteTime === 0) {
+      message.author.send(`Tu as mal exécuté la commande. \nUsage : ${this.usage}. \nTa commande : ${message}`);
+      message.delete();
+      return;
+    }
+
+    if (member.roles.cache.find(role => role === muteRole)) {
       message.author.send(`${member} est déjà mute ! Tu ne peux donc pas le mute de nouveau !`);
       message.delete();
       return;
@@ -40,7 +42,10 @@ module.exports = {
     member.roles.add(muteRole).then(() => {
 
       setTimeout(() => {
-        member.roles.remove(muteRole);
+        if (member.roles.cache.find(role => role === muteRole))
+          member.roles.remove(muteRole);
+        else
+          sanctionChannel.send(`**Erreur** Impossible d'enlever le grade 'mute' du membre : _${member.user.username}_, il n'est pas mute.`)
       }, muteTime);
 
     });
@@ -50,11 +55,11 @@ module.exports = {
       .setDescription(`Tu as été rendu muet sur le serveur Discord à cause d'un de tes messages. Un message a été automatiquement envoyé dans le salon des sanctions du Staff. Si nous te voyons trop souvent là bas, tu seras définitivement banni du serveur !`)
       .addFields({
         name: `Durée`,
-        value: `${args[1]}`,
+        value: `${ms(muteTime, { long: true })}`,
         inline: true
       }, {
         name: `Raison`,
-        value: `${why}`,
+        value: `${reason}`,
         inline: true
       })
       .setThumbnail(member.user.avatarURL())
@@ -66,6 +71,8 @@ module.exports = {
       muteEmbed.setDescription(`» ${member.user} a été sanctionné par ${message.author}`);
       sanctionChannel.send(muteEmbed);
     });
+
+    message.delete();
 
   }
 
